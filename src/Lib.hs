0addb7529a74b932118751037e148112fc8361cb
programmer <internetov.ru@gmail.com>
2020-12-25 23:29:51 +0300

    thoughts about overall loop type

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,18 @@
 module Lib
-    ( someFunc
+    (
+        someFunc,
+        Config(Config),
+        Settings(Settings),
+        Input(Input),
+        Action(name)
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Map as Map
+
+data Config = Config {tgToken :: String}
+data Settings = Settings {repeatsTg :: Map.Map String Int}
+data Input = Input {sender :: String, message :: String}
+data Action = Action {name :: String}
+
+someFunc :: Config -> Settings -> Input -> ([Action], Settings)
+someFunc cfg settings input = ([Action "wer", Action "spot"], settings)
