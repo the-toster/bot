5c25b7239dd2e14250a49f0380a690774aa8d0e3
programmer <internetov.ru@gmail.com>
2021-01-06 20:55:46 +0300

    add hie config, some test data

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
+import Test.Hspec
+import Model.MessageHandler
+import Config.Settings
+import Tg.Message
+import Model.User
+import Action
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =  hspec $ do
+  describe "handle" $ do
+    it "returns 2 echo messages on basic input" $
+      head (fst (handle someMessage settings)) `shouldBe` SendMessage TgMessage 
+
+settings :: Settings
+settings = initialSettings 2 "Hi, say /repeats to set repeats number"
+
+someMessage :: TgMessage
+someMessage = TgMessage
+                    (Just "123")
+                    (TgUser "123")
+                    (TgUser "321")
+                    "Body"
+                    Nothing
